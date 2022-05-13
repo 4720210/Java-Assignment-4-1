da7e3b4dfdd996bb1f893ccfd5f0dadd49a9b7c2
4720210 <103705497+4720210@users.noreply.github.com>
2022-05-13 10:41:44 +0900

    Update Prog41.java

--- src/main/java/Prog41.java
+++ src/main/java/Prog41.java
@@ -1,8 +1,14 @@
-public class Prog41 {
+public class Prog41
+{
+        public static void main(String[] args) {
+        
+            int [] numbers =new int [3]; 
+        
+        for(int i = 0; i <2; i++) {    
+         numbers[0] = Integer.parseInt(args[0]);   
 
-    public static void main(String[] args) {
-
+         System.out.println(numbers[0]);
     }
-
+}
 }
 
